46a6c2e7051f718795b9facfaf111df171f8044c
HuyB1910227 <huyb1910227@student.ctu.edu.vn>
2023-03-26 22:39:02 +0700

    Cai dat trang bao loi 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="page">
+        <p>
+            Đã xảy ra lỗi! Không thể tìm thấy trang. Trở về 
+            <router-link :to="{name: 'contactbook'}">trang chủ</router-link>
+        </p>
+    </div>
+</template>
